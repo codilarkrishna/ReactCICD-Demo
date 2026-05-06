87e3b133346974cd2eb7eb88670d46703b37c3a2
codilarkrishna <krishna.kk0106@gmail.com>
2026-05-06 15:52:36 +0530

    Initial react commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>Welcome to React</h1>
+          Edit <code>src/App.js</code>   save to reload and 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React as a begginer 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
